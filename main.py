8829e413c8067443ea5869268b11aef0dc732657
イム様 <113910380+Imu-D-sama@users.noreply.github.com>
2025-11-11 21:06:45 +0300

    Bump to ver 3.2

--- main.py
+++ main.py
@@ -2647,7 +2647,7 @@ elif ranBefore == True:
 
 # Check For Updates logic
 
-current_ver = 3.1
+current_ver = 3.2
 versionLabel = customtkinter.CTkButton(app, text="", anchor="w", fg_color="transparent")
 versionLabel.place(rely=0.956, relx=0.008)
 def updateApp():
